e0932347c8ffee0dbc0fde2430d2d1796387cf0d
andreynho2006 <andreynho2006@gmail.com>
2019-04-27 12:34:37 +0300

    map over persons state and return Persons with name and age and add DeleteHandler

--- burger-react-redux-app/src/App.js
+++ burger-react-redux-app/src/App.js
@@ -13,6 +13,13 @@ class App extends Component {
         showPersons: false
     }
 
+    deletePesonHandler = (personIndex) => {
+        const persons = this.state.persons;
+        persons.splice(personIndex, 1);
+        this.setState({persons: persons})
+
+    }
+
     togglePersonsHandler = () => {
         const doesShow = this.state.showPersons;
         this.setState({
@@ -44,17 +51,14 @@ class App extends Component {
         if (this.state.showPersons) {
             persons = (
                 <div>
-                    <Person  
-                        name={this.state.persons[0].name} 
-                        age={this.state.persons[0].age} />
-                    <Person 
-                        name={this.state.persons[1].name} 
-                        age={this.state.persons[1].age}
-                        changed={this.nameChangedHandler} >My hobbies: Racing
-                    </Person>
-                    <Person 
-                        name={this.state.persons[2].name} 
-                        age={this.state.persons[2].age} />
+                    {this.state.persons.map((person, index) => {
+                        return (
+                            <Person  
+                                click={() => this.deletePesonHandler(index)}
+                                name={person.name} 
+                                age={person.age} />
+                        )       
+                    })}
                 </div> 
             )
         }
