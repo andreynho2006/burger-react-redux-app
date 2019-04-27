8ead2fc0c7408d1ff37decc1a7ced8e1d33759b5
andreynho2006 <andreynho2006@gmail.com>
2019-04-27 09:55:47 +0300

    learn props and state

--- burger-react-redux-app/src/App.js
+++ burger-react-redux-app/src/App.js
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hi, I'm a React APp</h1>
-    </div>
-  );
+class App extends Component {
+    state = {
+        persons: [
+            { name: "Max", age: 28},
+            { name: "Manu", age: 29},
+            { name: "Simona", age: 30}
+        ],
+
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <h1>Hi, I'm a React APp</h1>
+                <p>This is really working</p>
+                <button>Switch Name</button>
+                <Person  name={this.state.persons[0].name} age={this.state.persons[0].age} />
+                <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My hobbies: Racing</Person>
+                <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+            </div>
+        );
+    }   
 }
 
 export default App;
---
+++ burger-react-redux-app/src/Person/Person.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const person = ( props ) => {
+    return (
+        <div>
+            <h1>Hi, I'm {props.name} I am {props.age} years old!</h1>
+            <p>{props.children}</p>
+        </div>
+        
+    );
+}
+
+export default person;
